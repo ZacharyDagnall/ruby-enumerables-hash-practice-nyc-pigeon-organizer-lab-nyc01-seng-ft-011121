17412056b06ee5868b99c6de1d323d893c887b8f
Zachary R Dagnall <zachary.dagnall@gmail.com>
2020-12-11 20:33:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,38 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  newdata = Hash.new
+  
+  #get all the names
+  names=[]
+  data.map { |att|
+    att[1].map { |spec|
+      names.concat(spec[1])
+    }
+  }
+  names.uniq!
+  
+  #turn names into keys in newdata, and initialize appropriate hash keys
+  names.each do |name|
+    newdata[name] = {color: [], gender: [], lives: []}
+  end
+  
+  #fill in colors 
+  names.each do |name|
+    data.each do |at|
+      at[1].each do |sp|
+        if sp[1].include?(name)
+          newdata[name][at[0]].push(sp[0])
+        end
+      end
+      binding.pry
+    end
+  end
+  #binding.pry
+ 
+  
+  newdata
 end
+
+
+
